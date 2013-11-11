0576dac7df754d7a4a7cb315c43102eb5e6eb259
Quentin <qmanthomas@gmail.com>
2013-11-11 11:39:37 -0800

    Added ch 5 excercises and renamed folders

---
+++ Excercises/ch1-5/examples.rb
@@ -0,0 +1,4 @@
+puts "Enter your full name:"
+full_name= gets.chomp
+puts "Your full name is " + full_name
+
---
+++ Excercises/ch1-5/favorite_number.rb
@@ -0,0 +1,4 @@
+puts "What is your favorite number?"
+number = gets.to_i
+number += 1
+puts "Here is your number with 1 added " + number.to_s
