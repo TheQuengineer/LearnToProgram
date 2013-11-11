5b70a80f21a207d432a67d61507da4bbec76b53e
Quentin <qmanthomas@gmail.com>
2013-11-11 11:04:31 -0800

    Added ch.2 excercise

---
+++ Excercises/ch2/numbers.rb
@@ -0,0 +1,9 @@
+hours = 8765.81
+puts "Hours in a year:  #{hours}"
+
+minutes = 5.25949e6
+puts "Minutes in a decade: #{minutes}"
+
+age_in_seconds = 918990171
+
+puts "My age in seconds: #{age_in_seconds}"
